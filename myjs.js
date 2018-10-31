c4bb08e6c470c52f66493cd62833b7a01426f181
bachlqth1807027 <bachlqth1807027@fpt.edu.vn>
2018-10-31 21:46:12 +0700

    mp3

--- myjs.js
+++ myjs.js
@@ -1,92 +1,30 @@
-var Mp3 = new Array(
-    {
-        id:'1540304588828',
-        name:'TÌnh Ca',
-        description:'Tình Ca',
-        singer:'Trọng Hiếu',
-        author:'Quốc Bảo',
-        thumbnail:'http://static.amusic.vn/amusic/albums/s2/10/82660/82660.jpg?v=1523961932',
-        link: 'https://s1.vocaroo.com/media/download_temp/Vocaroo_s1cqKcLqsxJ5.mp3',
-        memberId:'1538655637969',
-        createdAt:'2018-10-23T14:23:08.828+0000',
-        updatedAt:'2018-10-23T14:23:08.828+0000',
-        status:'1',
-    },
-        {
-            id:'1540304628575',
-            name:'Nếu',
-            description:"Hay",
-            singer:"Marzuz",
-            author:"Marzuz",
-            thumbnail:"http://vip.img.cdn.keeng.vn/sata11/singer/2018/01/08/uBoyTl6YIvKzjftFySPc5a52d1f0d0748.jpg",
-            link:"http://data2.chiasenhac.com/downloads/1725/2/1724970-05a36978/128/Neu%20-%20Onionn_%20Marzuz.mp3",
-            memberId:1539698396527,
-            createdAt:"2018-10-23T14:23:48.575+0000",
-            updatedAt:"2018-10-23T14:23:48.575+0000",
-            status:1
-        },
-        {
-            id:1540304709939,
-            name:"Sài Gòn Tôi Mưa ",
-            description:"Nằm trong bốn bức tường hai tư giờ\rChợt ngoài hiên xáo động tiếng mưa bất ngờ\rChiều nay giông đến sớm hơn mọi khi\rCầm ô tôi lê bước đi.\r\rĐường chật xe nháo nhào với cơn mưa rào\rNgười loay hoay cuống cuồng trú thân chốn nào\rÀo ào mưa rớt xuống đây\rSài Gòn lại thế.\r\r\rPre chorus:\r\rHm….\rCó ai nghe tiếng tôi tỉ tê \rHm….\rLúc cơn mưa chắn ai đường về\rHm….\rCó ai âm thầm để vài giọt lệ rơi\rGiữa chốn đông người, cảm thấy xa lạ, cảm thấy như vỡ oà.\r\rChorus:\r\rVì mình đã quá…cô đơn rồi\rGiữa phố hoa xốn xang nơi này\rTrong suốt những tháng ngày mỏng manh mỏng manh\rĐành phải tự thương vỗ về trấn an thân này.\rTự lau nước mưa và nước mắt từng chiều rơi xuống\rLàm sao đây, Sài Gòn mưa rơi, là mình lại yếu đuối.\r\r\rPre chorus:\r\rHm….\rCó ai nghe tiếng tôi tỉ tê \rHm….\rLúc cơn mưa chắn ai đường về\rHm….\rCó ai âm thầm để vài giọt lệ rơi\rGiữa chốn đông người, cảm thấy xa lạ, cảm thấy như vỡ oà.\r\rChorus:\r\rVì mình đã quá…cô đơn rồi\rGiữa phố hoa xốn xang nơi này\rTrong suốt những tháng ngày mỏng manh mỏng manh\rĐành phải tự thương vỗ về trấn an thân này.\rTự lau nước mưa và nước mắt từng chiều rơi xuống\rLàm sao đây, Sài Gòn mưa rơi, là mình lại yếu đuối.\r\rBridge:\rMuốn tan như mưa chẳng hoài buồn suy nghĩ\rMấy năm trôi qua tôi đã làm được gì\rPhố xa người lạ tôi muốn về nhà\rSài Gòn mưa, mưa, cho lòng người thêm ướt.\r\rChorus:\rVì mình đã hoài trôi giữa một khoảng không rất lạ\rVài ba người chắp tay mặt mừng\rNhưng đến khi xót xa biết ai mới là bạn.\r\rĐành phải tự thương vỗ về trấn an thân này.\rTự lau nước mưa và nước mắt từng chiều rơi xuống\rLàm sao đây, Sài Gòn mưa rơi, là mình lại yếu đuối.",
-            singer:" Nguyễn Kim Tuyên",
-            author:" Nguyễn Kim Tuyên",
-            thumbnail:"https://i1.sndcdn.com/artworks-000337189770-otd7tc-t500x500.jpg",
-            link:"https://od.lk/s/MTJfNjY3NTgxMV8/S%C3%A0i%20G%C3%B2n%20T%C3%B4i%20M%C6%B0a%20-%20Nguy%E1%BB%85n%20Kim%20Tuy%C3%AAn.mp3",
-            memberId:1538655637969,
-            createdAt:"2018-10-23T14:25:09.939+0000",
-            updatedAt:"2018-10-23T14:25:09.939+0000",
-            status:1
-        },
-        {
-            id:1540307622995,
-            name:"Kiss and Make up",
-            description:"Kiss and Make up",
-            singer:"Dua Lipa & BLACKPINK",
-            author:"Dua Lipa & BLACKPINK",
-            thumbnail:"https://pbs.twimg.com/media/DoiQ8WAXUAc2-lW.jpg",
-            link:"https://musicboss.org/get/music/20181020/musicbossorg_Dua_Lipa_Blackpink_-_Kiss_and_Make_Up_59804779.mp3",
-            memberId:1539103112760,
-            createdAt:"2018-10-23T15:13:42.995+0000",
-            updatedAt:"2018-10-23T15:13:42.995+0000",
-            status:1
-        },
-        {
-            id:1540310541059,
-            name:"All We Know",
-            description:"..Abc",
-            singer:"The Chainsmokers",
-            author:"The Chainsmokers",
-            thumbnail:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStP37BOuY6aLBDZ5mINo0kRZqeKNLiUCxvDxZ7LGFXf-jO7Cs8",
-            link:"https://c1-ex-swe.nixcdn.com/NhacCuaTui928/AllWeKnow-TheChainsmokersPhoebeRyan-4602228.mp3?st=ywk96BppwlX82JHikiDl3g&e=1540396889&t=1540310497234",
-            memberId:1540308101535,
-            createdAt:"2018-10-23T16:02:21.059+0000",
-            updatedAt:"2018-10-23T16:02:21.059+0000",
-            status:1
-        },
-        {
-            id:1540310846375,
-            name:"Beautifull Now",
-            description:"...ABC",
-            singer:"Zedd",
-            author:"Zedd",
-            thumbnail:"https://www.edmtunes.com/wp-content/uploads/2015/12/artworks-000121850691-14i9fg-t500x500_zpsfxnaapy9.jpg",
-            link:"https://c1-ex-swe.nixcdn.com/Unv_Audio46/BeautifulNow-ZeddJonBellion-4205279.mp3?st=fXA1UJCwSmuBiblmhOi4kg&e=1540397188&t=1540310789479",
-            memberId:1540308101535,
-            createdAt:"2018-10-23T16:07:26.375+0000",
-            updatedAt:"2018-10-23T16:07:26.375+0000",
-            status:1
-        },
-);
-var mymp3 = document.getElementById('mymp3');
-for (var i = 0;i < Mp3.length;i++){
-        var lisitMP3 = '    <div class="mp3" style="float: left;margin: 20px;">\n'+
-            '<audio controls autoplay="autoplay">\n' +
-            '        <source src=" '+ Mp3[i].link +' " type="audio/mpeg">\n' +
-            '    </audio>'+
-            '<h2>'+Mp3[i].name+'</h2>'+
-            '<h4>'+Mp3[i].description+'</h4>'+
-            '<h4>'+Mp3[i].singer+'</h4>'+
-            '<h4>'+Mp3[i].author+'</h4>'
-        mymp3.innerHTML += lisitMP3
+var MY_API = 'https://2-dot-backup-server-002.appspot.com/_api/v2/songs/get-free-songs';
+
+var xmlHttpRequest = new XMLHttpRequest();
+xmlHttpRequest.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var listSong = JSON.parse(this.responseText);
+        var content = '';
+        for (var i = 0; i < listSong.length; i++) {
+            content += '<div class="song-item">';
+            content += '<div class="song-index">' + (i + 1) + '</div>';
+            content += '<div class="song-thumbnail">';
+            content += '<img src="' + listSong[i].thumbnail + '" alt="">';
+            content += '</div>';
+            content += '<div class="song-infor">';
+            content += '<div class="song-name">' + listSong[i].name + '</div>';
+            content += '<div class="song-singer">' + listSong[i].singer + '</div>';
+            content += '</div>';
+            content += '<div class="song-control" onclick="playSong(\'' + listSong[i].link + '\', \'' + listSong[i].name + '\', \'' + listSong[i].singer + '\')">Play</div>';
+            content += '</div>';
+        }
+        document.getElementById('list-song').innerHTML = content;
+    }
+}
+xmlHttpRequest.open('GET', MY_API, true);
+xmlHttpRequest.send();
+
+function playSong(link, name, singer) {
+    document.getElementById('my-mp3').src = link;
+    document.getElementById('current-play-title').innerHTML = 'Current playing: ' + name + " - " + singer;
 }
